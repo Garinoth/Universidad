5fba91a13a63993151be691f09479b7776d9854f
Garinoth <cdavidrp@gmail.com>
2013-05-27 20:45:26 +0200

    Commit everything to clean up branches

---
+++ Pareja/DATSI/SD/JAVADFS.2012/dfs-fase2/compila_y_construye_JARS.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -x
+
+
+cd ..
+
+
+javac dfs/*.java
+
+
+jar cf dfs/dfs_cliente.jar dfs/DFSCliente.class dfs/DFSFicheroCliente.class dfs/DFSServicio.class dfs/DFSFicheroServ.class dfs/Bloque.class dfs/Cache.class dfs/Cache'$1'.class dfs/FicheroInfo.class dfs/DFSFicheroCallback.class dfs/DFSFicheroCallbackImpl.class dfs/DFSFicheroServImpl.class
+
+jar cf dfs/dfs_servidor.jar dfs/DFSServicio.class dfs/DFSFicheroServ.class dfs/DFSServicioImpl.class dfs/DFSFicheroServImpl.class dfs/Bloque.class dfs/FicheroInfo.class dfs/DFSFicheroCallback.class
+
+
+#jar cf dfs/dfs_servidor.jar dfs/Cerrojo.class dfs/CerrojoImpl.class dfs/Almacen.class dfs/AlmacenImpl.class dfs/CabeceraObjetoCompartido.class dfs/ObjetoCompartido.class dfs/FabricaCerrojos.class dfs/FabricaCerrojosImpl.class
